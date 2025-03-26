794b3dd6f175e320f89c9e666ef9b6ee7a45e419
Addison Crosbie <abacrosbie@gmail.com>
2025-03-26 16:48:58 +0000

    randomised colour when moused out

--- javascript.js
+++ javascript.js
@@ -1,31 +1,62 @@
-function createGrid(column, row){
+function createGrid(gridSize){
     //selects container
     const createDiv = document.querySelector(".container");
+
+    //clears grid
+    createDiv.innerHTML = '';
+
+    //sets grid size to fit squares
+    const squareSize = `${600 / gridSize}px`;
+
     //creates divs called gridSquare based on required input
-    for(let i = 0; i < row * column; i++) {
+    for(let i = 0; i < gridSize * gridSize; i++) {
         const div = document.createElement('div');
         div.classList.add("gridSquare")
+        div.style.width = squareSize;
+        div.style.height = squareSize;
         createDiv.appendChild(div);
     }
 }
 
-//calls function
-createGrid(16,16)
+//calls default functions
+let gridSize = 16;
+createGrid(gridSize);
+addHover();
 
 //adds listener events for squares
-document.querySelectorAll(".gridSquare").forEach(square => {
-    square.addEventListener("mouseover", runMouseOver);
-    square.addEventListener("mouseout", runMouseOut);
-});
-
+function addHover(){
+    document.querySelectorAll(".gridSquare").forEach(square => {
+        square.addEventListener("mouseover", runMouseOver);
+        square.addEventListener("mouseout", runMouseOut);
+    });
+}
 //colours squares when moused over
 function runMouseOver(e){
     e.target.style.backgroundColor = "rgb(40,40,40)"
-    console.log('Event Type: ' + e.type);
 }
 
 //colours squares when moused leaves box
 function runMouseOut(e){
-    e.target.style.backgroundColor = "rgb(200,200,200)"
-    console.log('Event Type: ' + e.type);
+    e.target.style.backgroundColor = randRGB();
+}
+
+//button clicked to create new grid
+document.getElementById('btn').addEventListener('click', buttonClick);
+function buttonClick(){
+    let gridSize = parseInt(prompt("Input Grid Size! (Up to 100x100)"))
+        if (isNaN(gridSize) || gridSize > 100 || gridSize <= 0){
+            alert("Select a number between 1 and 100!");
+        } else {
+            createGrid(gridSize);
+            addHover();
+        }
+}
+
+function randRGB(){
+    const min = 0;
+    const max = 255;
+    let randR = Math.floor(Math.random() * max - min +1) + min;
+    let randG = Math.floor(Math.random() * max - min +1) + min;
+    let randB = Math.floor(Math.random() * max - min +1) + min;
+    return "rgb("+randR+","+randG+","+randB+")"
 }
