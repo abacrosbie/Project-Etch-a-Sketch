23c910f2f88c7d3b98e2ab42e612b5963f3c6a53
Addison Crosbie <abacrosbie@gmail.com>
2025-03-26 15:06:11 +0000

    added hover functionality to js to colour grid grey with a black indicator of where cursor currently is

--- javascript.js
+++ javascript.js
@@ -1,10 +1,31 @@
 function createGrid(column, row){
-
-    const createDiv = document.querySelector("div");
-    for(let i = 1; i < row * column; i++) {
+    //selects container
+    const createDiv = document.querySelector(".container");
+    //creates divs called gridSquare based on required input
+    for(let i = 0; i < row * column; i++) {
         const div = document.createElement('div');
+        div.classList.add("gridSquare")
         createDiv.appendChild(div);
     }
 }
 
+//calls function
 createGrid(16,16)
+
+//adds listener events for squares
+document.querySelectorAll(".gridSquare").forEach(square => {
+    square.addEventListener("mouseover", runMouseOver);
+    square.addEventListener("mouseout", runMouseOut);
+});
+
+//colours squares when moused over
+function runMouseOver(e){
+    e.target.style.backgroundColor = "rgb(40,40,40)"
+    console.log('Event Type: ' + e.type);
+}
+
+//colours squares when moused leaves box
+function runMouseOut(e){
+    e.target.style.backgroundColor = "rgb(200,200,200)"
+    console.log('Event Type: ' + e.type);
+}
